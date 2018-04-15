480127bdaaced71bb987f7e81f923200b9eb9791
m <mark.alex@web.de>
2018-04-15 14:26:34 +0200

    initial

--- Trello.R
+++ Trello.R
@@ -3,7 +3,11 @@ setwd('/Users/m/code/Python/Trello')
 
 trello <- read.csv("trello")
 logDays <- log(trello$days)
-trello$urgency <- 1-logDays/max(logDays)
+trello$urgency <- 1-(logDays-min(logDays))/(max(logDays)-min(logDays))
+
+logComplexity <- log(trello$complexity)
+
+trello$relativeComplexity <- 100-100/(trello$complexity^0.5)
 
 p <- plot_ly(trello, x = ~urgency, y = ~importance, type = 'scatter', mode = 'markers+text',
              marker = list(size = ~complexity*5, opacity = 0.5),
